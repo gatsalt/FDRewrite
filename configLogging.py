50b511f93a82a31a030ba2580ab26cd05ac70940
gatsalt <51649702+gatsalt@users.noreply.github.com>
2019-07-19 18:44:25 -0400

    Add files via upload

---
+++ configLogging.py
@@ -0,0 +1,13 @@
+
+import logging
+import datetime
+
+def configLogging(settings):
+	t = datetime.datetime.now()
+	logFile = '{}{}.ReportingScripts.log'.format(settings['loggingFolder'], t.strftime('%Y.%m.%d'))
+	print('ExtractSSC logging to: {}'.format(logFile))
+	print('to view try Get-Content -Path \"{}\" -Wait'.format(logFile))
+	logging.basicConfig(level=logging.INFO,
+	    filename=logFile,
+	    filemode='a', 
+	    format='%(asctime)s - %(levelname)s - %(message)s')
